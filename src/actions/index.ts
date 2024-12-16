d08eb6533a1610b8cacddd9841cef9ed9e66a0dc
Spee12 <sphuntsho900@gmail.com>
2024-12-16 17:34:28 +0600

    feat(apps): update on fixes

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -20,3 +20,26 @@ export async function deleteSnippet(id: number) {
     });
     redirect('/snippet/details');
 }
+
+export async function createSnippet(formState: {message: string},formData: FormData) {
+    // This needs to be a server action
+    const title = formData.get("title");
+    const code = formData.get("code");
+
+    if (typeof title !== 'string' || title.length < 3) {
+        return {
+            message: 'Title must be longer'
+        };
+    }
+    if (typeof code !== 'string' || code.length < 10) {
+        return {
+            message: 'Title must be longer'
+        };
+    }
+
+    const snappet = await db.snippet.create({
+      data: {title,code},
+    });
+    
+    redirect('/snippet/details');
+  }
--- src/app/snippet/page.tsx
+++ src/app/snippet/page.tsx
@@ -1,22 +1,13 @@
+'use client';
+
 import SharedComponent from '@/components/shared-component';
-import { db } from '@/db ';
-import { redirect } from 'next/navigation';
+import { useFormState } from 'react-dom';
+import * as actions from '@/actions';
 
 export default function CreateSnippetPage() {
-    async function createSnippet(formData: FormData) {
-      // This needs to be a server action
-      "use server";
-      const title = formData.get("title");
-      const code = formData.get("code");
-
-      const snappet = await db.snippet.create({
-        data: {title,code},
-      });
-      redirect('/snippet/details');
-    }
-
+  const [formState, action] = useFormState(actions.createSnippet, {message: ''});
     return (
-      <form action={createSnippet}>
+      <form action={action}>
         <SharedComponent imageUrl={'/realibility.jpg'} imageAlt='Relibility Image' titile='Bhutan Is Committed To Sustainable Tourism '>
             <div className='container mx-auto flex flex-col gap-4 mt-12'>
                 <div className='flex flex-col justify-center items-center gap-4'>
@@ -29,6 +20,9 @@ export default function CreateSnippetPage() {
                         <textarea className='border rounded p-2 w-[90%] ml-4' name='code' id='code' />
                     </div>
                 </div>
+                {
+                  formState.message ? <div className='w-fit m-auto p-2 bg-red-200 border rounded bourder-red-400'>{formState.message}</div> : null
+                }
                <div className='flex justify-center items-center '>
                 <button className='rounded p-2 bg-blue-200' type='submit'>
                         Create
